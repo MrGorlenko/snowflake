4d4b2a1323ccff8b924047cf5ca5dd087e7e2ecb
Artem Gorlenko <52908127+MrGorlenko@users.noreply.github.com>
2023-08-22 20:12:09 +0400

    version tag to header

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+export default function Document() {
+	return (
+		<Html>
+			<Head>
+				<meta name='version' content='1.0.1'></meta>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
